9e1ad48612166e8e74bdf78a54cd2d581c57ccfa
karlabeni12 <karla.beni12@utexas.edu>
2016-12-04 11:25:10 -0600

    order and scroll

--- clouds.js
+++ clouds.js
@@ -1,3 +1,15 @@
 $(document).ready( function () {
     $('#table_id').DataTable();
 } );
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "order": [[ 3, "desc" ]]
+    } );
+} );
+$(document).ready(function() {
+    $('#example').DataTable( {
+        "scrollY":        "200px",
+        "scrollCollapse": true,
+        "paging":         false
+    } );
+} );
