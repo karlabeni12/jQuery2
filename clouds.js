40032abb469c727e47d90e9c191615fbec51a4ce
karlabeni12 <karla.beni12@utexas.edu>
2016-12-04 11:22:54 -0600

    tableland

--- clouds.js
+++ clouds.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-  $('#sky').jQlouds();
-});
-$('#sky').jQlouds({
-  minClouds: 3, // minimum amount of clouds
-  maxClouds: 5, // maximum amount of clouds
-});
-$('#sky').jQlouds({
-  wind: true
-});
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
